d0b948c04eaa286be0e2bdd4e982b0a7b185f7ff
Rakhan Dawood <rakhandawood@Rakhans-MacBook-Pro.local>
2025-02-19 23:05:13 +0530

    Update JavaScript for Webflow integration

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ export default {
       }
   
       // Fetch your JavaScript from GitHub
-      const githubURL = 'https://raw.githubusercontent.com/RakiDawood1/my-webflow-app/main/app.js';
+      const githubURL = 'https://raw.githubusercontent.com/RakiDawood1/my-webflow-app/main/js/pp.js';
       
       try {
         const response = await fetch(githubURL);
